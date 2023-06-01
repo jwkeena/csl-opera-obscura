de6dc5e528bffe4aa4ff5af688b24afe6b85a6b1
jwkeena <jwkeena@gmail.com>
2023-06-01 14:24:21 -0400

    Updates

--- precache-manifest.3d536a1d2a983082e6a90d54f9d5c077.js
+++ precache-manifest.3d536a1d2a983082e6a90d54f9d5c077.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b74188fee9087ec91ad0087cab5af141",
+    "revision": "e901fbae188f7f48d193292dd023f8f2",
     "url": "/csl-opera-obscura/index.html"
   },
   {
-    "revision": "3d5d7d7cd6022d6cd97a",
+    "revision": "689f7d602e38755a5a19",
     "url": "/csl-opera-obscura/static/css/main.b844b49e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/csl-opera-obscura/static/js/2.fabdfe80.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d5d7d7cd6022d6cd97a",
-    "url": "/csl-opera-obscura/static/js/main.8602f03d.chunk.js"
+    "revision": "689f7d602e38755a5a19",
+    "url": "/csl-opera-obscura/static/js/main.538d7b6e.chunk.js"
   },
   {
     "revision": "f24c559c91d0bb77cad2",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/csl-opera-obscura/precache-manifest.a03830a1ca02fa7b80178525db3c61a7.js"
+  "/csl-opera-obscura/precache-manifest.3d536a1d2a983082e6a90d54f9d5c077.js"
 );
 
 self.addEventListener('message', (event) => {
