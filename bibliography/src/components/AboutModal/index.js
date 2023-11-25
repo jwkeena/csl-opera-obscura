151aeae5686078169a4183d086d4d0be912bc1ef
jwkeena <jwkeena@gmail.com>
2023-11-24 22:32:24 -0500

    Update Childhood's End blurb variants

--- bibliography/src/components/AboutModal/index.js
+++ bibliography/src/components/AboutModal/index.js
@@ -21,7 +21,7 @@ class AboutModal extends Component {
                     </div>
                     </div>
                 </a>
-                    <p className="center-align nudge-up"><b><a href="https://jwkeena.github.io/" target="_blank" rel="noopener noreferrer" title="See my portfolio">Code and design by Justin Keena</a> · last updated 11.13.23</b></p>
+                    <p className="center-align nudge-up"><b><a href="https://jwkeena.github.io/" target="_blank" rel="noopener noreferrer" title="See my portfolio">Code and design by Justin Keena</a> · last updated 11.24.23</b></p>
                 <div 
                     ref={ (modal) => {this.modal = modal} }
                     id="about"
